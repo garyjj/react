31ec9bc024a12bcaa6dc2381f28437e3f9311eb2
garaza <garaza@altimetrik.com>
2018-12-30 00:52:58 +0530

    Initial Commit

---
+++ src/main/java/com/altimetrik/microservice/react/ReactApplication.java
@@ -0,0 +1,14 @@
+package com.altimetrik.microservice.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactApplication.class, args);
+	}
+
+}
+
